38e9c0b488cf0121832e8504d2d7939a16394ab7
moritz-hanke <kicolenidman@gmail.com>
2023-07-24 18:25:24 +0200

    ..

--- exec/generate-plots-BSS-time-limits.R
+++ exec/generate-plots-BSS-time-limits.R
@@ -1,11 +1,15 @@
 #' Plotting results of Best Subset Selection based
 #' on MIO Formulation by Bertsimas et al. (2016) and the Gurbi Solver
 #' with respect to different time limits and certified solutions
-#' 
+#' and Firgures 30-56 in the Appendixy (number of certified runs)
 
 # Load necessary packages 
-library(ggpubr)
+library(dplyr)
+library(tibble)
+library(ggplot2)
 library(tidyverse)
+library(ggpubr)
+library(reshape2)
 
 
 
@@ -173,7 +177,7 @@ ggarrange(ggarrange(g1, g2, ncol=2, legend = FALSE), g3, g4, ncol=1,
           legend="bottom")
 
 # save the plots
-ggsave("./plots/Time_comparison_blocklowfirst70_snr042.png",
+ggsave("./plots/Figure_11.png",
        width = 18, 
        height = 24, units = "cm", dpi = 300)
 
@@ -344,6 +348,221 @@ ggarrange(ggarrange(g1, g2, ncol=2, legend = FALSE), g3, g4, ncol=1,
           legend="bottom")
 
 # save the plots
-ggsave("./plots/Time_comparison_semisynlowfirst_snr042.png",
+ggsave("./plots/Appendix_Figure_57.png",
        width = 18, 
        height = 24, units = "cm", dpi = 300)
+
+
+
+
+
+#### Plots for the Appendix Figures 30-56 ####
+
+#' @param CORR vector of correlation structures
+#' @param BETA vector of non-zero positions
+#' @param DIM dimensionality of problem
+#' @param SNR vecotr of signal-to-noise values
+
+#' counter is needed for labaling the figures according to the appendix.
+counter <- 29
+#' If only low dimensional data is available, give warning
+if(!all(c("high", "medium", "low") %in% DIM)){
+  cat("WARNINBG: There is no high/medium-dimensional setting data available.\n")
+  cat("Will only plot results for low-dimensional settings.\n")
+  cat("Please follow the instructions in the README and masterscript\n")
+  cat("to generate/dowload high/medium-dimensional result\n")
+}else{
+  DIM <- c("low", "medium", "high") # order of dimensionality in the Appendix
+}
+
+
+for(Dim in DIM){
+  
+  for(Corr in CORR){
+    
+    for(Beta in BETA){
+      if(Beta == "first"){
+        Beta_title <- "adjacent"
+      }else{
+        Beta_title <- "equally distributed"
+      }
+      
+      # we only need one beta positioning if there is no correlation between
+      #. any predictor
+      if(Corr == "independent" & Beta == "spread"){
+        next
+      }
+      
+      # load the raws results
+      raw_results <- 
+        readRDS(paste("./results/raw_results_",
+                      Dim, "_",
+                      Beta, "_",
+                      Corr, ".RDS",
+                      sep="")
+        )
+      
+      
+      
+      names(raw_results) <- c("job.id",         "problem"    ,    "algorithm"    ,  "n"           ,
+                              "p"           ,   "s"           ,   "dimensionality", "corr_type" ,    
+                              "rho"         ,   "beta_type"    ,  "snr"        ,    "k"      ,       
+                              "alpha"      ,    "result" )
+      
+      
+      
+      
+      
+      SNR <- unique(raw_results$snr)
+      
+      RHO <- unique(raw_results$rho)
+      
+      
+      for(Rho in RHO){
+        
+        
+        
+        out_snr <- lapply(SNR, function(Snr){
+          
+          
+          indices <- which(raw_results$dimensionality == Dim & 
+                             raw_results$rho == Rho & 
+                             raw_results$snr == Snr & 
+                             raw_results$corr_type == Corr & 
+                             raw_results$beta_type == Beta)
+          
+          out <- lapply(indices, function(x){
+            raw_results$result[[x]]
+            
+          })
+          
+          out <- do.call(rbind, out)
+          
+          out_BSS <- 
+          
+          no_enets <- as_tibble(out[is.na(out$alpha) | out$alpha==1,]) %>% 
+            mutate(method = replace(method, alpha == 1, "lasso"))
+          
+          enets <- as_tibble(out[out$alpha>0 & out$alpha<1 & out$method=="enet",])
+          enets$alpha <- round(enets$alpha,2)
+          enets <- enets %>% 
+            # uncomment if more Enet versions are desired
+            mutate(method = replace(method, alpha == 0.1, "enet_0.1")) %>% 
+            # mutate(method = replace(method, alpha == 0.2, "enet_0.2")) %>%
+            # mutate(method = replace(method, alpha == 0.3, "enet_0.3")) %>%
+            # mutate(method = replace(method, alpha == 0.4, "enet_0.4")) %>%
+            mutate(method = replace(method, alpha == 0.5, "enet_0.5")) %>%
+            # mutate(method = replace(method, alpha == 0.6, "enet_0.6")) %>%
+            # mutate(method = replace(method, alpha == 0.7, "enet_0.7")) %>%
+            # mutate(method = replace(method, alpha == 0.8, "enet_0.8")) %>%
+            mutate(method = replace(method, alpha == 0.9, "enet_0.9"))
+          
+          enets <- enets[enets$method != "enet",]
+          
+          one_setting <- bind_rows(enets, no_enets)
+          
+          # dismiss hybrid (we implemented our idea of a two step procedure by a
+          # pre-selection via Enet/Lasso followed by BSS; these results are not
+          # part of the paper "Variable selection in linear regression models: 
+          # choosing the best subset is not always the best choice" and are 
+          # therefore dismissed from the following analysis.)
+          one_setting <- one_setting[one_setting$method != "enet_bs_hybrid", ]
+          
+          one_setting$method <- factor(one_setting$method,
+                                       levels = 
+                                         c("enet_0.1",
+                                           # "enet_0.2", 
+                                           # "enet_0.3", 
+                                           # "enet_0.4", 
+                                           "enet_0.5", 
+                                           # "enet_0.6", 
+                                           # "enet_0.7", 
+                                           # "enet_0.8", 
+                                           "enet_0.9", 
+                                           "lasso",
+                                           "fs",
+                                           "bs" ))
+          levels(one_setting$method)[levels(one_setting$method) == "enet_0.1"] <- 
+            "Enet 0.1"
+          # levels(one_setting$method)[levels(one_setting$method) == "enet_0.2"] <- 
+          #   "Enet 0.2"
+          # levels(one_setting$method)[levels(one_setting$method) == "enet_0.3"] <- 
+          #   "Enet 0.3"
+          # levels(one_setting$method)[levels(one_setting$method) == "enet_0.4"] <- 
+          #   "Enet 0.4"
+          levels(one_setting$method)[levels(one_setting$method) == "enet_0.5"] <- 
+            "Enet 0.5"
+          # levels(one_setting$method)[levels(one_setting$method) == "enet_0.6"] <- 
+          #   "Enet 0.6"
+          # levels(one_setting$method)[levels(one_setting$method) == "enet_0.7"] <- 
+          #   "Enet 0.7"
+          # levels(one_setting$method)[levels(one_setting$method) == "enet_0.8"] <- 
+          #   "Enet 0.8"
+          levels(one_setting$method)[levels(one_setting$method) == "enet_0.9"] <- 
+            "Enet 0.9"
+          levels(one_setting$method)[levels(one_setting$method) == "lasso"] <- 
+            "Lasso"
+          levels(one_setting$method)[levels(one_setting$method) == "fs"] <- 
+            "FSS"
+          levels(one_setting$method)[levels(one_setting$method) == "bs"] <- 
+            "BSS"
+          
+          names(one_setting)[c(1,12,13,14)] <- c("Method", 
+                                                 "Corresponding Recall", 
+                                                 "Corresponding Precision", 
+                                                 "Best possible F1")
+          
+          one_setting <- one_setting %>% 
+            pivot_longer(c("Best possible F1",
+                           "Corresponding Recall",
+                           "Corresponding Precision"), 
+                         names_to = 'Metric', 
+                         values_to = 'Value')
+          
+          p <- unique(one_setting$p)
+          s <- unique(one_setting$s)
+          
+          one_setting
+        })
+        out_snr <- do.call(bind_rows, out_snr)
+        
+        p <- unique(out_snr$p)
+        s <- unique(out_snr$s)
+        
+        pic <- ggplot(out_snr , 
+                      aes(x = as.factor(snr), y = Value, fill = Method))+
+          geom_boxplot()+
+          facet_wrap( ~ Metric, ncol = 1)+
+          ylim(0,1)+
+          scale_fill_manual(values=c(
+            "#FF99CC",
+                     "#FF66FF",
+                     "#B266FF",
+                     "#FF3333",
+                     "#0080FF",
+                     "#00CC00"
+          )) + 
+          xlab("Signal-to-noise ratrio τ")+
+          theme(plot.title = element_text(size=9))
+        
+        counter <- counter +1
+        ggsave(paste("./plots/Appendix_Figure_",
+                     counter,
+                     ".png", sep=""), 
+               width = 18, 
+               height = 18, units = "cm", dpi = 300)
+        
+        
+        
+        
+      }
+      
+      
+    }
+    
+  }
+  
+}
+
+
+
