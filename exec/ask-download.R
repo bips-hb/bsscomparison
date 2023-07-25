5be93d673ecb2132db7a7ba49ec3be2cd62b35c6
moritz-hanke <kicolenidman@gmail.com>
2023-07-25 16:32:28 +0200

    changed download description

--- exec/ask-download.R
+++ exec/ask-download.R
@@ -5,7 +5,7 @@ answered <- FALSE
 while (!answered) { 
   
   # ask whether to run the simulation with different time limits
-  time_study <- menu(title = "Do you want to download the intermediate results? Required for creating the related plots.\nYou can download the files manually under\nhttps://zenodo.org/record/8139859/files/BestSubsetResults.zip?download=1 \nand safe the file in ./results", 
+  time_study <- menu(title = "Do you want to download the intermediate results? Required for creating the related plots.\nYou can download the files manually under\nhttps://zenodo.org/record/8139859/files/BestSubsetResults.zip?download=1 \nand safe and unzip the file to ./results", 
                      choices = c("yes", 
                                  "no",
                                  "downloaded manually",
@@ -26,7 +26,7 @@ while (!answered) {
          }, 
          
          "3" = {
-           cat("--> I have downloaded the results and saved in ./results to generate plots for medium and high dimensional settings\n")
+           cat("--> I have downloaded the results and extracted to ./results to generate plots for medium and high dimensional settings\n")
            create_medium_high_dimensional_plots <- TRUE
            answered <- TRUE
          }, 
